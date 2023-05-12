e26f15b6b43577ce8a1a72570e2866013e8ec0fa
Historia Montague <historia.montague@outlook.com>
2023-05-12 10:25:00 -0600

    Finished Lab

--- javascript/bubble_sort.js
+++ javascript/bubble_sort.js
@@ -1,5 +1,20 @@
 function bubbleSort(arr) {
   // type your code here
+  let sorted = false
+  while(!sorted) {
+    sorted = true
+    for(let i = 0; i < arr.length - 1; i++) {
+      if(arr[i] > arr[i + 1]) {
+        //normal swap wasn't working, used bitwise XOR to swap instead
+        sorted = false
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]]
+        // arr[i] ^= arr[i + 1]
+        // arr[i + 1] ^= arr[i]
+        // arr[i] ^= arr[i + 1]
+      }
+    }
+  }
+  return arr
 }
 
 if (require.main === module) {
@@ -26,4 +41,8 @@ if (require.main === module) {
 module.exports = bubbleSort;
 
 // Please add your pseudocode to this file
+// Create a bool that is true by default and is set to false if the swap happens while iterating over the array
+// while the bool is false continue to iterate over the entire array
+// If the swap never happens, the array is sorted and returned
 // And a written explanation of your solution
+// Space complexity is O(n) time complexity is O(n^2)
